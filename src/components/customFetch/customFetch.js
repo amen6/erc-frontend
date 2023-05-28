df05c7cb6cf9f372da424f5f1371508eef3624bd
Amen <jamen.jamen30@gmail.com>
2023-05-28 20:52:33 +0300

    custom fetch/hook

---
+++ src/components/customFetch/customFetch.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+// import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  //   const token = Cookies.get("userToken");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await axios.get(
+          `${process.env.REACT_APP_URL}${url}`
+          // {
+          //   headers: { Authorization: `Bearer ${token}` },
+          // }
+        );
+        setData(res.data);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.get(`${process.env.REACT_APP_URL}${url}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setIsLoading(false);
+  };
+  return { data, isLoading, error, reFetch };
+};
+
+export default useFetch;
